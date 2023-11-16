b182a0abe7c30861ba05c2b1717976fae377faf4
rohithreddybc <rohithreddybc98@gmail.com>
2023-11-16 01:32:30 -0500

    auto incrementing ID with @GeneratedValue annotation

--- studentDAL/src/main/java/com/rohith/student/dal/entities/student.java
+++ studentDAL/src/main/java/com/rohith/student/dal/entities/student.java
@@ -2,6 +2,8 @@ package com.rohith.student.dal.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,6 +12,7 @@ import jakarta.persistence.Table;
 public class student {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	@Column(name="sname")
 	private String name;
